4352427463dd4cad2736ed0fb88562791e095e01
SajeedAninda <sajaninda@gmail.com>
2024-07-12 19:34:51 +0600

    Get the search value

--- src/Components/IpTracker.jsx
+++ src/Components/IpTracker.jsx
@@ -4,6 +4,14 @@ import arrowIcon from "../assets/icon-arrow.svg"
 
 
 const IpTracker = () => {
+
+    let handleSearchValue = (e) => {
+        e.preventDefault();
+
+        let ipAddress = e.target.ip_address.value;
+        console.log(ipAddress);
+    }
+
     return (
         <div>
             <div className='upperDiv h-[40vh] flex py-8 items-center flex-col'>
@@ -11,8 +19,8 @@ const IpTracker = () => {
                     Ip Address Tracker
                 </h1>
 
-                <form className='w-full flex justify-center items-center mt-4'>
-                    <input className='bg-white py-5 px-4 rounded-l-2xl w-[40%] h-[65px] focus:border-none' type="text" id="ip-address" name="ip-address" placeholder='Search for any IP Address or Domain' />
+                <form onSubmit={handleSearchValue} className='w-full flex justify-center items-center mt-4'>
+                    <input className='bg-white py-5 px-4 rounded-l-2xl w-[40%] h-[65px] focus:border-none' type="text" id="ip_address" name="ip_address" placeholder='Search for any IP Address or Domain' />
                     <button className='text-white py-4 px-5 rounded-r-2xl bg-black h-[65px] hover:bg-[#2B2B2B]' type='submit'>
                         <img className='w-[15px]' src={arrowIcon} alt="" />
                     </button>
